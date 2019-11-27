86405d321f1c011b71d73eb969a410f548c4e924
root <root@AXEL-PC.localdomain>
2019-11-27 19:30:48 +0100

    Modif du main

--- main.swift
+++ main.swift
@@ -5,18 +5,16 @@ On ne peut doubler une forme que si l’on a joué la précédente soi-même.
 Le premier joueur qui pose la quatrième forme différente dans une ligne, 
 une colonne ou une zone remporte immédiatement la partie, qu’importe à qui appartiennent les autres pièces de ce coup gagnant. */
 
-
-
-//-------------------------------Fonctions pour augmenter la lisibilité du code-----------------------------------//
-func demande_nom(x : Int)->String {
-	print("Joueur " + String(x) +" veuillez taper votre nom : ")
+/*-------------------------------Fonctions pour augmenter la lisibilité du code-----------------------------------*/
+func demande_nom(num : String)->String {
+	print("Joueur " + num + " veuillez taper votre nom : ")
 	let input = readLine()
 	guard var nom : String = input {
 		return nom
 	}
 	else{
 		print("Oops je ne peux pas accepter ce nom !")
-		return demande_nom()
+		return demande_nom(x : x)
 	}
 }
 
@@ -28,17 +26,36 @@ func changerActif(actif : Joueur, j1 : Joueur, j2 : Joueur)->Joueur {
 		return j1
 	}
 }
-//---------------------------------------------------------------------------------------------------------------//
+
+func choisirEntier(type : String) -> Int{
+	print("Veuillez choisir une " + type + " : ")
+	let input = readLine()
+	guard var result : Int = input {
+		//On verifie que l'entier est compris entre 1 et 4 car il n'y a que 4 lignes et 4 colones
+		if (result >= 1) && (result <= 4){
+			return result
+		}
+		else{
+			print("Oops cet indice n'est pas valide...")
+			return choisirEntier(type : type)
+		}
+	}
+	else{
+		print("Oops je ne peux pas accepter cela !")
+		return choisirEntier(type : type)
+	}
+}
+/*---------------------------------------------------------------------------------------------------------------*/
 
 print("Vous venez de lancer le Quantik !\nPressez [ENTER] pour continuer")
 let pass = readLine()//Pour n'afficher le reste que si le joueur reagit
 
 //Definition et affectation des joueurs et du jeu
-var nom1 : String = demande_nom(1)
-var nom2 : String = demande_nom(2)
+var nom1 : String = demande_nom(num : "1")
+var nom2 : String = demande_nom(num : "2")
 
-var joueur1 : Joueur = Joueur(nom:nom1, couleur:True)
-var joueur2 : Joueur = Joueur(nom:nom2, couleur:False)
+var joueur1 : Joueur = Joueur(nom : nom1, couleur : 31)
+var joueur2 : Joueur = Joueur(nom : nom2, couleur : 34)
 
 var jeu : Jeu = Jeu()
 
@@ -54,30 +71,30 @@ else {
 //La partie commence
 print("La partie peut commencer...")
 
-while (!jeu.partieFinie{
+while (!jeu.partieFinie){
 	//On passe au joueur suivant
 	actif = changerActif(actif : actif, j1 : joueur1, j2 : joueur2)
 	//Le joueur peut jouer
 	if (jeu.peutJouer(actif)){
 		//On affiche le plateau et le deck du joueur actif
-		jeu.afficheJeu()
+		jeu.affichePlateau()
 		actif.afficheDeck()
-		var piece : Piece
-		var position : [Int](2)
 		//Le joueur actif doit choisir son emplacement et sa piece
-		piece = actif.choisirPiece()
-		position = actif.choisirEmplacement()
-		//Si ce n'est pas possible de poser la piece sur l'emplacement alors le joueur doit recommencer en choisissant un emplacement et une piece
+		var piece = actif.choisirPiece()
+		var x : Int = choisirEntier(type : "ligne")
+		var y : Int = choisirEntier(type : "colone")
+		//Si ce n'est pas possible de poser la piece sur l'emplacement alors le joueur doit recommencer la saisie
 		while(!jeu.peutPoser(pos : position, pie : piece)){
 			//On affiche le plateau et le deck du joueur actif
-			jeu.afficheJeu()
+			jeu.affichePlateau()
 			actif.afficheDeck()
 			piece = actif.choisirPiece()
-			position = actif.choisirEmplacement()
+			x : Int = choisirEntier(type : "ligne")
+			y : Int = choisirEntier(type : "colone")
 		}
 		//Le joueur actif pose sa piece, il faut donc la mettre sur le plateau et l'enlever de son deck
 		jeu.posePiece(pos : position, pie: piece)
-		jeu.afficheJeu()
+		jeu.affichePlateau()
 	}
 	//Le joueur ne peut pas jouer donc la partie est finie
 	else{
@@ -86,4 +103,4 @@ while (!jeu.partieFinie{
 	}
 }
 //La partie est finie, on affiche le vainqueur
-print("Bravo " + actif.nom + "tu as gagné la partie !")
+print("Bravo " + actif.nom + ", tu as gagné la partie !")
