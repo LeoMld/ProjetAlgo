466a29c26e57f0402a2939e0c2b7c3f13dcd0abd
Leo <lele.mollard@gmail.com>
2019-11-28 08:58:21 +0100

    specifs

--- Case.swift
+++ Case.swift
@@ -2,8 +2,10 @@ protocol TCase {
 	/*creer_case : -> Case
 	creer une case */
 	init()
+	
 	/* contenu_case : Case -> Piece?
-renvoi le contenu d'une case */
-	var contenu_case : Piece? {get}
+	renvoi le contenu d'une case 
+	Post : La case ne peut contenir que soit une piece soit vide*/
+	var contenu_case : Piece? {get set}
 }
 
--- Joueur.swift
+++ Joueur.swift
@@ -1,6 +1,6 @@
 protocol TJoueur{
 	/*CreerJoueur(String x Bool) -> Joueur
-	Post : ne pas oublier d'initialiser le deck du joueur qui possèdera 		donc 8 Pieces en début de partie,
+	Post : ne pas oublier d'initialiser le deck du joueur qui 			possèdera donc 8 Pieces placée dans des cases en début de partie,
 	Pre : la couleur représente une couleur existante en unicode */
 	init(nom : String, couleur: String)
 
@@ -11,7 +11,7 @@ protocol TJoueur{
 
 	/* supprimer_piece : Joueur x Piece -> Joueur
 	Pre : Choisir une pièce existante dans le deck du joueur  
-	Post : la piece sera supprimée du deck du joueur donc cette piece sera 	remplacée par nil dans le deck */
+	Post : la piece sera supprimée du deck du joueur donc cette piece 	sera remplacée par nil dans le deck */
 	mutating func supprimer_piece(piece : Piece)
 
 	/* afficher_deck : Joueur -> Joueur 
@@ -20,7 +20,7 @@ protocol TJoueur{
 
 	/* choisir_piece : Joueur -> Piece 
 	choisi une piece dans le deck du joueur
-	Pre : donner un entier qui représente une case où une pièce est 	dispo 	dans le deck (1≤Int≤8*/
+	Pre : donner un entier qui représente une case où une pièce est 	dispo 	dans le deck (1≤Int≤8)*/
 	func choisir_piece(pos : Int) -> Piece
 }
 
