dee028922defa9530b292dee7b5cae256f4de5ee
axel.duval <axel.duval@IGTP4-PC09.isim.intra>
2019-11-28 10:40:46 +0100

    modif du main

--- main.swift
+++ main.swift
@@ -6,15 +6,15 @@ Le premier joueur qui pose la quatrième forme différente dans une ligne,
 une colonne ou une zone remporte immédiatement la partie, qu’importe à qui appartiennent les autres pièces de ce coup gagnant. */
 
 /*-------------------------------Fonctions pour augmenter la lisibilité du code-----------------------------------*/
-func demande_nom(num : String)->String {
+func demandeNom(num : String)->String {
 	print("Joueur " + num + " veuillez taper votre nom : ")
 	let input = readLine()
-	guard var nom : String = input {
+	if let nom : String = input {
 		return nom
 	}
 	else{
 		print("Oops je ne peux pas accepter ce nom !")
-		return demande_nom(x : x)
+		return demande_nom(num : num)
 	}
 }
 
@@ -27,12 +27,12 @@ func changerActif(actif : Joueur, j1 : Joueur, j2 : Joueur)->Joueur {
 	}
 }
 
-func choisirEntier(type : String) -> Int{
+func choisirEntier(type : String, inf : Int, sup : Int) -> Int{
 	print("Veuillez choisir une " + type + " : ")
 	let input = readLine()
-	guard var result : Int = input {
+	if let result : Int = input {
 		//On verifie que l'entier est compris entre 1 et 4 car il n'y a que 4 lignes et 4 colones
-		if (result >= 1) && (result <= 4){
+		if (result >= inf) && (result <= sup){
 			return result
 		}
 		else{
@@ -45,17 +45,34 @@ func choisirEntier(type : String) -> Int{
 		return choisirEntier(type : type)
 	}
 }
+
+func affichePlateau(jeu : Jeu){
+	//Fonction qui affiche le plateau
+}
+
+func afficheDeck(deck : [Piece]){
+	//Fonction qui affiche les pieces dispo
+	var numbers : String = ""
+	var pieces : String = ""
+	var i : Int
+	for i in 0..< deck.count{
+		numbers = numbers + String(i+1) + "\t"
+		pieces = pieces + "\u{1B}[" + deck[i].couleur +"m\u{" + deck[i].forme +"}\u{1B}[0m\t"
+	}
+	print(numbers)
+	print(pieces)
+}
 /*---------------------------------------------------------------------------------------------------------------*/
 
 print("Vous venez de lancer le Quantik !\nPressez [ENTER] pour continuer")
 let pass = readLine()//Pour n'afficher le reste que si le joueur reagit
 
 //Definition et affectation des joueurs et du jeu
-var nom1 : String = demande_nom(num : "1")
-var nom2 : String = demande_nom(num : "2")
+var nom1 : String = demandeNom(num : "1")
+var nom2 : String = demandeNom(num : "2")
 
-var joueur1 : Joueur = Joueur(nom : nom1, couleur : 31)
-var joueur2 : Joueur = Joueur(nom : nom2, couleur : 34)
+var joueur1 : Joueur = Joueur(nom : nom1, couleur : "31")//31 est le code unicode pour rouge
+var joueur2 : Joueur = Joueur(nom : nom2, couleur : "34")//34 est le code unicode pour bleu
 
 var jeu : Jeu = Jeu()
 
@@ -76,30 +93,33 @@ while (!jeu.partieFinie){
 	actif = changerActif(actif : actif, j1 : joueur1, j2 : joueur2)
 	//Le joueur peut jouer
 	if (jeu.peutJouer(actif)){
+		print(actif.nom + " c'est à toi de jouer !")
 		//On affiche le plateau et le deck du joueur actif
-		jeu.affichePlateau()
-		actif.afficheDeck()
+		affichePlateau()
+		actif.afficheDeck(actif.piecesDispo())
 		//Le joueur actif doit choisir son emplacement et sa piece
-		var piece = actif.choisirPiece()
-		var x : Int = choisirEntier(type : "ligne")
-		var y : Int = choisirEntier(type : "colone")
+		var pos : Int = choisirEntier(type : "position de choix de piece dans le deck ", inf : 1, sup : actif.nbrePieceDispo())
+		var piece = actif.choisirPiece(pos : pos)
+		var x : Int = choisirEntier(type : "ligne", inf : 1, sup : 4)
+		var y : Int = choisirEntier(type : "colone", inf : 1, sup : 4,)
 		//Si ce n'est pas possible de poser la piece sur l'emplacement alors le joueur doit recommencer la saisie
 		while(!jeu.peutPoser(ligne : x, col : y, p : piece)){
 			//On affiche le plateau et le deck du joueur actif
-			jeu.affichePlateau()
-			actif.afficheDeck()
-			var pos : Int = choisirEntier(type : "position de choix de 				piece dans le deck ")
+			affichePlateau(jeu : jeu)
+			actif.afficheDeck(actif.piecesDispo())
+			pos = choisirEntier(type : "position de choix de piece dans le deck ", inf : 1, sup : actif.nbrePieceDispo())
 			piece = actif.choisirPiece(pos : pos)
-			x : Int = choisirEntier(type : "ligne")
-			y : Int = choisirEntier(type : "colone")
+			x = choisirEntier(type : "ligne", inf : 1, sup : 4)
+			y = choisirEntier(type : "colone", inf : 1, sup : 4)
 		}
 		//Le joueur actif pose sa piece, il faut donc la mettre sur le plateau et l'enlever de son deck
-		jeu.posePiece(ligne : x, col : y, p : piece)
-		jeu.affichePlateau()
+		jeu.posePiece(ligne : x, col : y, piece : piece)
+		actif.supprimePiece(piece : piece)
+		affichePlateau()
 	}
 	//Le joueur ne peut pas jouer donc la partie est finie
 	else{
-		jeu.partieFinie = True
+		jeu.partieFinie = true
 		actif = changerActif(actif : actif, j1 : joueur1, j2 : joueur2)
 	}
 }
