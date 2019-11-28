3bc8649c53a5618bed4bea83e12dc3dee9e8837e
Leo <lele.mollard@gmail.com>
2019-11-28 08:52:44 +0100

    specifs

--- Jeu.swift
+++ Jeu.swift
@@ -1,10 +1,13 @@
 protocol TJeu{
 	/* CreerJeu: -> Jeu 
-	Création d'un jeu */
+	Création d'un jeu 
+	Post : créé le plateau contenant des case qui seroont donc toutes 	vides*/
 	init()
 
-	/* Fonctions qui vont renvoyer true si il y a bien 4 formes différente dans une ligne/colonne/zone
-	Pre : donner une ligne et une colone connue */
+	/*
+	Fonctions qui vont indiuer si il y a 4 formes différentes dans une 	ligne/colonne/zone donnée 
+	Pre : donner une ligne et une colone connue (compisent entre 1 et 	4)
+	Post : Fonctions qui vont renvoyer true si il y a bien 4 formes 	différente dans une ligne/colonne/zone*/
 	/* ligne_complete : Jeu x Int -> Bool */
 	func ligne_complete(ligne : Int) -> Bool
 	/* colonne_complete : Jeu x Int -> Bool */
@@ -13,30 +16,36 @@ protocol TJeu{
 	func zone_complete(ligne : Int, col : Int) -> Bool
 
 
-	/* Fonctions qui vont envoyer false si la ligne/colonne/zone contient une pièce de même forme et de couleur différente que la pièce courante 
-	Pre : donner une ligne et une colone connue (comprisent entre 1 et 4) */
-	/* ligne_contient : Jeu x Int x Piece -> Bool */
+	/* Fonctions qui vont envoyer false si la ligne/colonne/zone 		contient une pièce de même forme et de couleur différente que la 	pièce courante 
+	Pre : donner une ligne et une colone connue (comprisent entre 1 et 	4)
+	Post : renvoi true si la piece est contenu sur le plateau dans la 	ligne/colonne/zone indiquée, false sinon */
+	/* ligne_contient : Jeu x Int x Piece -> Bool 
+	Pre : Int représente la ligne du plateau où l'on veut chercher */
 	func ligne_contient(ligne : Int, piece : Piece) -> Bool
-	/* colonne_contient : Jeu x Int x Piece -> Bool */
+	/* colonne_contient : Jeu x Int x Piece -> Bool 
+	Pre : Int représente la colonne du plateau où l'on veut chercher*/
 	func colonne_contient(col : Int, piece : Piece) -> Bool
-	/* zone_contient : Jeu x Int x Piece -> Bool */
+	/* zone_contient : Jeu x Int x Int x Piece -> Bool 
+	Pre : Int x Int représente la ligne x colonne du plateau où l'on 	veut chercher*/
 	func zone_contient(ligne : Int, col : Int, piece : Piece) -> Bool
 
-	/* peut_poser : Jeu x Int x Int x Piece -> Bool
-	envoi true si le joueur peut poser une pièce a une position 
+	/* peutPoser : Jeu x Int x Int x Piece -> Bool
+	indique si un joueur peut poser une piece a une position
+	Pre : Int x Int représente la ligne / colonne du plateau et Piece 	une case du deck contenant une pièce donc une case différente de 	nil
+	Post : envoi true si le joueur peut poser une pièce a une position 
 	 */
 	func peutPoser(ligne : Int, col : Int, piece : Piece) -> Bool
 
-	/* peut_jouer : Jeu x Joueur -> Bool
+	/* peutJouer : Jeu x Joueur -> Bool
 	indique si le joueur peut jouer 
 	Post : envoi true si un joueur peut jouer ou bien false si il ne 	peut plus poser de piece, en effet si le joueur est bloqué (aucune 	de ses pièces ne peut être poser sur le plateau et il n'a pas 		gagné */
 	func peutJouer(j : Joueur) -> Bool
 
 	/* pose_piece : Jeu x Int x Int x Piece -> Jeu 
-	Pose une piece sur le jeu
-	Pre : La case à cette position doit être vide
-	Post : on vérifie si le joueur a gagné (ligne/colonne/zone est 		complète)*/
-	mutating func posePiece(ligne : Int, col : Int, piece : Piece)
+	Pose une piece sur le plateau du jeu
+	Pre : Int x Int représente la position (ligne/colonne) du jeu, la 	case à cette position doit être vide. actif représente le joueur 	actif à ce tour de jeu. Piece représente une case du deck 			contenant une pièce donc une case différente de nil
+	Post : on vérifie si le joueur a gagné (ligne/colonne/zone est 		complète) et on retire la pièce du deck du joueur actif*/
+	mutating func posePiece(ligne : Int, col : Int, piece : Piece, actif : Joueur)
 
 	/* case_vide : Jeu x Int x Int -> Bool
 	indique si la case du plateau est vide
--- Piece.swift
+++ Piece.swift
@@ -1,18 +1,19 @@
 protocol TPiece{
-	/* Type Piece est une juxtaposition de 2 propriétés: Couleur (Bool) et une forme(String)
+	/*
 	Création d'une piece:
-	init : String x Bool -> Piece
-	Pre : String représente une forme existante (rond, triangle, cylindre, 	carré) sinon la creation échoue */
-	init(forme : String, couleur : Bool )
+	init : String x String -> Piece
+	Pre : String représente une forme existante (rond, triangle, 		cylindre, carré) sinon la creation échoue et String une couleur 	existante en unicode */
+	init(forme : String, couleur : String )
 	
-	/* couleur : Piece -> Bool
-	renvoi la couleur d'une piece */
-	var couleur : Bool {get}
+	/* couleur : Piece -> String
+	renvoi la couleur d'une piece
+	Pre : string représente une couleur existante en unicode */
+	var couleur : String {get}
 
 	
 	/* forme : Piece -> String
 	renvoi la forme d'une piece 
-	Post : renvoi une forme connue sous forme d'un string */
+	Post : renvoi une forme connue sous forme (rond, triangle, 			cylindre, carré) d'un string */
 	var forme : String {get}
 }
 
